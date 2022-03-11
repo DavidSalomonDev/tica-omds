d47153d368577033a4b7b78bcd51da7b8fa5539e
david_chromeOS <david@buho.academy>
2022-03-10 21:33:53 -0600

    🎉 project files

---
+++ src/pages/App.js
@@ -0,0 +1,7 @@
+import "styles/App.css";
+
+function App() {
+	return <div className="App">TICA OMDS</div>;
+}
+
+export default App;
---
+++ src/services/team.js
@@ -0,0 +1,10 @@
+const team = [
+	{
+		id: 1,
+		ldap: "davidsalomon",
+		name: "David Salomón",
+		stackOverflowId: "17311782",
+	},
+];
+
+export default team;
