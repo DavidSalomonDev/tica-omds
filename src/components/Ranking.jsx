16cf31a470a1f3e29396d8f7c05a6239765be83b
David Salomón <me@davidsalomon.dev>
2022-03-11 09:31:42 -0600

    adding team members to api request

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -1,32 +1,37 @@
 import { useEffect, useState } from "react";
-import useFetch from "hooks/useFetch";
+import team from "services/team";
 
 const Ranking = () => {
-	const [users, setUsers] = useState([]);
-	const url =
-		"https://api.stackexchange.com/2.3/users/17311782?order=desc&sort=reputation&site=stackoverflow";
+  const [users, setUsers] = useState([]);
 
-	const fetchUsers = async () => {
-		const response = await fetch(url);
-		const data = await response.json();
-		setUsers(data.items);
-	};
+  const teamIds = team.map((user) => user.stackOverflowId);
+  const usersIds = teamIds.join(";");
 
-	useEffect(() => {
-		fetchUsers();
-	}, []);
+  const url = `https://api.stackexchange.com/2.3/users/${usersIds}?order=desc&sort=reputation&site=stackoverflow`;
 
-	const usersData = users.map((user) => {
-		return (
-			<div key={user.account_id}>
-				<img src={user.profile_image} alt={user.display_name}></img>
-				<h1>{user.display_name}</h1>
-				<h2>Reputation: {user.reputation}</h2>
-			</div>
-		);
-	});
+  const fetchUsers = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setUsers(data.items);
+  };
 
-	return <div>{usersData.length === 0 ? <h1>No users</h1> : usersData}</div>;
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const usersData = users.map((user) => {
+    return (
+      <div key={user.account_id}>
+        <br />
+        <img src={user.profile_image} alt={user.display_name}></img>
+        <h1>{user.display_name}</h1>
+        <h2>Reputation: {user.reputation}</h2>
+        <hr />
+      </div>
+    );
+  });
+
+  return <div>{usersData.length === 0 ? <h1>No users</h1> : usersData}</div>;
 };
 
 export default Ranking;
--- src/services/team.js
+++ src/services/team.js
@@ -1,10 +1,46 @@
 const team = [
-	{
-		id: 1,
-		ldap: "davidsalomon",
-		name: "David Salomón",
-		stackOverflowId: "17311782",
-	},
+  {
+    id: 1,
+    ldap: "davidsalomon",
+    name: "David Salomón",
+    stackOverflowId: "17311782"
+  },
+  {
+    id: 2,
+    ldap: "gabrielalejand",
+    name: "Gabo",
+    stackOverflowId: "17390082"
+  },
+  {
+    id: 3,
+    ldap: "laraportillo",
+    name: "Fer",
+    stackOverflowId: "17926591"
+  },
+  {
+    id: 4,
+    ldap: "ricardojosev",
+    name: "Richie",
+    stackOverflowId: "17926477"
+  },
+  {
+    id: 5,
+    ldap: "yancymelany",
+    name: "Yancy",
+    stackOverflowId: "10344196"
+  },
+  {
+    id: 6,
+    ldap: "lorenagomez",
+    name: "Lore",
+    stackOverflowId: "17926478"
+  },
+  {
+    id: 7,
+    ldap: "danirivera",
+    name: "Dani",
+    stackOverflowId: "12306687"
+  }
 ];
 
 export default team;
